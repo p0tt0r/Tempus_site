33ce81eb3482759091f4457964cdde35da2dfd19
p0tt0r <shchetinine@MacBook-Air.local>
2026-06-25 22:33:33 +0300

    Fix education navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,42 +13,24 @@ export default function Header() {
           <Link to="/">Главная</Link>
 
           <div className="nav-dropdown">
-            <Link to="/sveden">Сведения об образовательной организации</Link>
+            <Link to="/sveden/osnovnye-svedeniya">Сведения об образовательной организации</Link>
 
             <div className="dropdown-menu">
               <div className="dropdown-menu">
                 <Link to="/sveden/osnovnye-svedeniya">Основные сведения</Link>
-                <Link to="/sveden/struktura-i-organy-upravleniya-obrazovatelnoj-organizacziej">
-                  Структура и органы управления
-                </Link>
+                <Link to="/sveden/struktura-i-organy-upravleniya">Структура и органы управления</Link>
                 <Link to="/sveden/dokumenty">Документы</Link>
                 <Link to="/sveden/obrazovanie">Образование</Link>
                 <Link to="/sveden/rukovodstvo">Руководство</Link>
-                <Link to="/sveden/pedagogicheskij-sostav">Педагогический состав</Link>
-                <Link to="/sveden/materialno-tehnicheskoe-obespechenie">
-                  Материально-техническое обеспечение
-                </Link>
-                <Link to="/sveden/platnye-obrazovatelnye-uslugi">
-                  Платные образовательные услуги
-                </Link>
-                <Link to="/sveden/finansovo-hozyajstvennaya-deyatelnost">
-                  Финансово-хозяйственная деятельность
-                </Link>
-                <Link to="/sveden/vakantnye-mesta-dlya-priema">
-                  Вакантные места для приема
-                </Link>
-                <Link to="/sveden/stipendii-i-mery-podderzhki">
-                  Стипендии и меры поддержки
-                </Link>
-                <Link to="/sveden/obrazovatelnye-standarty">
-                  Образовательные стандарты и требования
-                </Link>
-                <Link to="/sveden/mezhdunarodnoe-sotrudnichestvo">
-                  Международное сотрудничество
-                </Link>
-                <Link to="/sveden/organizacziya-pitaniya">
-                  Организация питания
-                </Link>
+                <Link to="/sveden/pedagogicheskiy-sostav">Педагогический состав</Link>
+                <Link to="/sveden/materialno-tehnicheskoe-obespechenie">Материально-техническое обеспечение</Link>
+                <Link to="/sveden/platnye-obrazovatelnye-uslugi">Платные образовательные услуги</Link>
+                <Link to="/sveden/finansovo-hozyaystvennaya-deyatelnost">Финансово-хозяйственная деятельность</Link>
+                <Link to="/sveden/vakantnye-mesta-dlya-priema">Вакантные места для приема</Link>
+                <Link to="/sveden/stipendii-i-mery-podderzhki">Стипендии и меры поддержки</Link>
+                <Link to="/sveden/obrazovatelnye-standarty-i-trebovaniya">Образовательные стандарты и требования</Link>
+                <Link to="/sveden/mezhdunarodnoe-sotrudnichestvo">Международное сотрудничество</Link>
+                <Link to="/sveden/organizatsiya-pitaniya">Организация питания</Link>
               </div>
             </div>
           </div>
