b4b7aff8e7935c62e747373dafc74f0b2573ab99
p0tt0r <shchetinine@MacBook-Air.local>
2026-06-26 04:10:48 +0300

    New animations

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { useNavigate } from "react-router-dom";
+
 
 
 
